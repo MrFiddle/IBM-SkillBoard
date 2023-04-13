aeed9dfec3a8d1f8938eae155a590b044cc86b8f
Gael <gaelrodguez@hotmail.com>
2023-04-13 11:26:04 -0600

    (client): Log in form, not connected to back, styles missing

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./Login.css";
 import Logo from "../Logo";
-import { Formik } from "formik";
+import { Formik, Form, Field } from "formik";
+import { Credentials } from "../../../lib/types";
 
-const Login = () => {
+interface Props {
+  handleLogin: (values: Credentials) => string;
+}
+
+const Login = ({ handleLogin }: Props) => {
   return (
     <div className="general">
       <div className="half left">
@@ -17,6 +22,36 @@ const Login = () => {
       <div className="half right">
         <div className="login-box">
           <p className="text-xl font-bold">LOG IN</p>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            onSubmit={(values: Credentials) => {
+              handleLogin(values);
+            }}
+          >
+            <Form>
+              <div>
+                <label htmlFor="email">Email</label>
+                <Field
+                  id="email"
+                  name="email"
+                  placeholder="cristianoronaldodossantosaveiro@ibm.com"
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Password</label>
+                <Field
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Muchas gracias aficion, esto es para vosotros SIUUUUUU"
+                />
+              </div>
+              <button type="submit">Log in</button>
+            </Form>
+          </Formik>
         </div>
       </div>
     </div>
--- client/src/components/LoginContainer.tsx
+++ client/src/components/LoginContainer.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Login from "./Login/Login";
+import { Credentials } from "../../lib/types";
 
 const LoginContainer = () => {
-  return <Login />;
+  const handleLogin = (values: Credentials) => {
+    console.log(values);
+    return "Zy";
+  };
+
+  return <Login handleLogin={handleLogin} />;
 };
 
 export default LoginContainer;
