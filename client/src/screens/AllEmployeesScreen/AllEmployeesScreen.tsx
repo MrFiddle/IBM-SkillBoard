87041fc36377af93aede909bfefc706c9e9448fd
Emilio Gonzalez <emiliogonmar@Emilios-MacBook-Pro-4.local>
2023-04-17 09:03:10 -0600

    (client): All screens created

---
+++ client/src/screens/AllEmployeesScreen/AllEmployeesScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllEmployeesScreen = () => {
+  return <div>AllEmployeesScreen</div>;
+};
+
+export default AllEmployeesScreen;
---
+++ client/src/screens/MyTeamsScreen/MyTeamsScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyTeamsScreen = () => {
+  return <div>MyTeamsScreen</div>;
+};
+
+export default MyTeamsScreen;
