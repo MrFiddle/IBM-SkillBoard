ddaa6f57a76da2264b96e6cdc891a40b51e3a083
han rodríguez <han@gonzalpi.com>
2023-04-18 14:08:25 -0600

    Field validation on certificates

--- backend-dashboard-2/app/models/certificate.rb
+++ backend-dashboard-2/app/models/certificate.rb
@@ -5,4 +5,15 @@ class Certificate
   field :name, type: String
   field :type, type: String
   field :expiration_date, type: Date
+
+  validates :name,
+  presence: true,
+  length: { minimum: 3 }
+
+  validates :type,
+  presence: true,
+  inclusion: {
+    in: %w(ibm industry),
+    message: "must be either ibm or industry"
+  }
 end
