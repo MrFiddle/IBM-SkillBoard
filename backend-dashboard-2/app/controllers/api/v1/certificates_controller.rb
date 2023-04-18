a2424c34138ab55baeefc4b8aa56f2ad3087b520
han rodríguez <han@gonzalpi.com>
2023-04-18 10:55:09 -0600

    Add certificate controller

--- backend-dashboard-2/app/controllers/api/v1/certificates_controller.rb
+++ backend-dashboard-2/app/controllers/api/v1/certificates_controller.rb
@@ -1,2 +1,52 @@
 class Api::V1::CertificatesController < ApplicationController
+    before_action :set_certificate, only: %i[ show update destroy ]
+
+    # GET /certificates
+    def index
+      @certificates = Certificate.all
+  
+      render json: @certificates
+    end
+  
+    # GET /certificates/1
+    def show
+      render json: @certificates
+    end
+  
+    # POST /certificates
+    def create
+      @certificate = Certificate.new(name: params[:name], expiration_date: params[:expiration_date], type: params[:type])
+  
+      if @certificate.save
+        render json: @certificate, status: :created, location: api_v1_certificate_url(@certificate)
+      else
+        render json: @certificate.errors, status: :unprocessable_entity
+      end
+      
+    end
+  
+    # PATCH/PUT /certificates/1
+    def update
+      if @certificate.update(certificate_params)
+        render json: @certificate
+      else
+        render json: @certificate.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /certificates/1
+    def destroy
+      @certificate.destroy
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_certificate
+        @certificate = Certificate.find(params[:id])
+      end
+  
+      # Only allow a list of trusted parameters through.
+      def certificate_params
+        params.fetch(:certificate).permit(:name, :expiration_date, :type)
+      end
 end
