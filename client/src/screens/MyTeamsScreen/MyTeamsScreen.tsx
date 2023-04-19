cba677c7a3df28c401484f76e0ad05dc90784312
Emilio Gonzalez <emiliogonmar@Emilios-MacBook-Pro-4.local>
2023-04-19 09:49:38 -0600

    (client): Added Header to all screens

--- client/src/screens/MyTeamsScreen/MyTeamsScreen.tsx
+++ client/src/screens/MyTeamsScreen/MyTeamsScreen.tsx
@@ -1,5 +1,11 @@
+import Header from "../../components/Header/Header";
+
 const MyTeamsScreen = () => {
-  return <div className="background-screen">MyTeamsScreen</div>;
+  return (
+    <div className="background-screen">
+      <Header title={"My Team"} />
+    </div>
+  );
 };
 
 export default MyTeamsScreen;
