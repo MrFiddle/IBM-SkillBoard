c1366cd8fc5ccf92d6c2f40a2ca322bc6bc8ec0f
Alyxhelpme <A01640585@itesm.mx>
2023-04-19 07:15:22 -0600

    Current cors route available in application.yml

--- backend-dashboard-2/config/initializers/cors.rb
+++ backend-dashboard-2/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*" #Where React app is
+    origins "<%= ENV["REACT_ROUTE"] %>" #Where React app is
 
     resource "*",
       headers: :any,
