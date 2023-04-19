9b8f3fc937961b1fa07defda5f04bfb6109e45f9
han rodríguez <han@gonzalpi.com>
2023-04-18 22:54:01 -0600

    Allow CORS from * for dev, fix #55

--- backend-dashboard-2/config/initializers/cors.rb
+++ backend-dashboard-2/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://localhost:3001" #Where React app is
+    origins "*" #Where React app is
 
     resource "*",
       headers: :any,
