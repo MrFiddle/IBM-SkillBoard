f7e13c4010741c71e1ff43f09196c43ee43ec309
Alyxhelpme <A01640585@itesm.mx>
2023-04-20 09:14:37 -0600

    FullControl

--- backend-dashboard-2/app/controllers/api/v1/auth_controller.rb
+++ backend-dashboard-2/app/controllers/api/v1/auth_controller.rb
@@ -7,18 +7,17 @@ class Api::V1::AuthController < ApplicationController
 
     uri = URI("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword")
     
-
-    res = Net::HTTP.post_form(
-      uri,
-      'key' => ENV['FIREBASE_KEY'],
-      'email' => email,
-      'password' => password,
-    )
-
-    uri = URI.parse("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword")
-
     http = Net::HTTP.new(uri.host,uri.port)
     request = Net::HTTP::Get.new(uri.request_uri)
+
+    request.set_form_data({'key'=>ENV['FIREBASE_KEY'], 'email' => email, 'password' => password})
+    # res = Net::HTTP.post_form(
+    #   uri,
+    #   'key' => ENV['FIREBASE_KEY'],
+    #   'email' => email,
+    #   'password' => password,
+    # )
+
 
     respond = http.request(request)
 
