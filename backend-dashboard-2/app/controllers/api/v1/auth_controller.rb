2b37a450776652ae9a60c1565d5a3593dc320d4c
Alyxhelpme <A01640585@itesm.mx>
2023-04-19 21:26:45 -0600

    Reconfigured Auth POST README to maria, testing cookie send to data

--- backend-dashboard-2/app/controllers/api/v1/auth_controller.rb
+++ backend-dashboard-2/app/controllers/api/v1/auth_controller.rb
@@ -18,6 +18,7 @@ class Api::V1::AuthController < ApplicationController
 
     if res.is_a?(Net::HTTPSuccess)
       session[:user_id] = data['localId']
+      res['Set-Cookie']
       head :ok
     else
       render status: :unauthorized, json: { error: "Bad login credentials" }
