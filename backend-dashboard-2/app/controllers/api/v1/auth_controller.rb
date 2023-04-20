6f7d3217b3dfd8a6c3c545f6fb7f685df66e08c0
Alyxhelpme <A01640585@itesm.mx>
2023-04-19 23:47:29 -0600

    FRONT END RECEIVED AAAAH, CRIES

--- backend-dashboard-2/app/controllers/api/v1/auth_controller.rb
+++ backend-dashboard-2/app/controllers/api/v1/auth_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::AuthController < ApplicationController
 
     if res.is_a?(Net::HTTPSuccess)
       session[:user_id] = data['localId']
-      render status: :ok, json: {msg: "rendering?", idToken: data['idToken']}
+      render status: :ok, json: {idToken: data['idToken']}
     else
       render status: :unauthorized, json: { error: "Bad login credentials" }
     end
