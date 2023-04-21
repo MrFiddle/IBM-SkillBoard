7ff96135fd70a66a0c127f9dedf9f030d54d6b8c
Alyxhelpme <A01640585@itesm.mx>
2023-04-21 13:08:15 -0600

    ay caray no agarra

--- backend-dashboard-2/config/initializers/cors.rb
+++ backend-dashboard-2/config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173/'
+    origins 'localhost:5175'
+
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: true,
+      origin: true
   end
 end
