09bf9fedc91f07e2a8cf809c35df30e64d69613a
Juan Pablo <jppd0657@gmail.com>
2023-04-20 23:57:55 -0600

    (client): deleted console log on DashboardTable.tsx and also fixed some warnings

--- client/src/components/DashboardTable/DashboardTable.tsx
+++ client/src/components/DashboardTable/DashboardTable.tsx
@@ -8,20 +8,19 @@ interface Props {
 }
 
 const DashboardTable = ({ certificates }: Props) => {
-  console.log(certificates)
   return (
     <div className='flex flex-col w-[100%] h-[100%] DashboardTable'>
 
       <div className='DashboardTable_Filters'>
-        <select className='rounded-md p-2 drop-shadow-xl pr-16'>
-          <option value='' disabled selected hidden className='font-semibold'>
-            Industry
-          </option>
-          <option value=''>All</option>
-          <option value=''>IBM</option>
-          <option value=''>Microsoft</option>
-          <option value=''>Google</option>
-        </select>
+      <select className='rounded-md p-2 drop-shadow-xl pr-16' defaultValue='' >
+        <option value='' disabled hidden className='font-semibold'>
+          Industry
+        </option>
+        <option value=''>All</option>
+        <option value='IBM'>IBM</option>
+        <option value='Microsoft'>Microsoft</option>
+        <option value='Google'>Google</option>
+      </select>
 
         <p className='font-semibold'>Name</p>
         <p className='font-semibold'>Type</p>
@@ -29,8 +28,8 @@ const DashboardTable = ({ certificates }: Props) => {
         <p className='font-semibold'>Industry compatiblity</p>
       </div>
       <div className='overflow-auto'>
-        {certificates['certificates'].map((certificate) => (
-          <CertificationRow key={certificate.id} certificate={certificate} />
+        {certificates['certificates'].map((certificate, index) => (
+          <CertificationRow key={`${certificate.id}-${index}`} certificate={certificate} />
         ))}
       </div>
 
