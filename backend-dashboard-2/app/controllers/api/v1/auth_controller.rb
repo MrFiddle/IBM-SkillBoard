93e4afdefb9ca3e673c4f500394b87d1e8bb19f2
Alyxhelpme <A01640585@itesm.mx>
2023-04-21 16:12:09 -0600

    erasing that thing

--- backend-dashboard-2/app/controllers/api/v1/auth_controller.rb
+++ backend-dashboard-2/app/controllers/api/v1/auth_controller.rb
@@ -18,7 +18,6 @@ class Api::V1::AuthController < ApplicationController
 
     if res.is_a?(Net::HTTPSuccess)
       session[:user_id] = data['localId']
-      binding.pry # <-- add this line
       render status: :ok, json: {idToken: data['idToken']}
     else
       render status: :unauthorized, json: { error: "Bad login credentials" }
