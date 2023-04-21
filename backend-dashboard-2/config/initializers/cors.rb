c89f72c4b4c7b6c243c3b821e53d14b5527e10c5
Alyxhelpme <A01640585@itesm.mx>
2023-04-21 13:11:14 -0600

    I just dont know how to route

--- backend-dashboard-2/config/initializers/cors.rb
+++ backend-dashboard-2/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5175'
+    origins 'http://localhost:5173'
 
     resource "*",
       headers: :any,
