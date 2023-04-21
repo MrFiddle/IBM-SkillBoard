9191acea8d481f702b4c84a6cc7c5bec524d4921
Alyxhelpme <A01640585@itesm.mx>
2023-04-21 17:29:52 -0600

    idented block

--- backend-dashboard-2/config/initializers/cors.rb
+++ backend-dashboard-2/config/initializers/cors.rb
@@ -15,10 +15,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true,
       expose: ['Authorization']
 
-      if request.headers['HTTP_ORIGIN']
-        # Set the Access-Control-Allow-Origin header to the HTTP_ORIGIN of the request
-        # This will allow the requesting domain to access the API
-        headers['Access-Control-Allow-Origin'] = request.headers['HTTP_ORIGIN']
-      end
+    if request.headers['HTTP_ORIGIN']
+      # Set the Access-Control-Allow-Origin header to the HTTP_ORIGIN of the request
+      # This will allow the requesting domain to access the API
+      headers['Access-Control-Allow-Origin'] = request.headers['HTTP_ORIGIN']
+    end
   end
 end
