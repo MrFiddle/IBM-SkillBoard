36c01b50c7f3e869b88ebdb061bd86e3ecb6bf78
Alyxhelpme <A01640585@itesm.mx>
2023-04-25 10:02:38 -0600

    ENV routes

--- backend-dashboard-2/config/initializers/cors.rb
+++ backend-dashboard-2/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:5173','http://68.183.111.241:8080']
+    origins ENV["REACT_ROUTE"].split(",")
 
     resource "*",
       headers: :any,
