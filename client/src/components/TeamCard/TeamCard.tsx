dc56777bd7647b022c947f56b617a4df14f97595
Juan Pablo <jppd0657@gmail.com>
2023-04-25 10:22:57 -0600

    (client): made scrollbar always visible and changed font-size button

--- client/src/components/TeamCard/TeamCard.tsx
+++ client/src/components/TeamCard/TeamCard.tsx
@@ -51,7 +51,7 @@ const TeamCard = ({ team }: Props) => {
       <div className="flex flex-row justify-start w-full">
         <h2 className="font-bold text-[1.2rem]">{team.team_name}</h2>
       </div>
-      <div className="flex flex-col justify-start w-full overflow-auto team-card-employees">
+      <div className="flex flex-col justify-start w-full overflow-scroll team-card-employees">
         {dummyData.map((user) => (
           <TeamRow user={user} />
         ))}
