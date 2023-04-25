46c8e851e5c4f9849ad70890a316e7dfde66c6cb
han rodríguez <han@gonzalpi.com>
2023-04-25 12:01:43 -0600

    Permit id instead of name in user params

--- backend-dashboard-2/app/controllers/api/v1/users_controller.rb
+++ backend-dashboard-2/app/controllers/api/v1/users_controller.rb
@@ -48,6 +48,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user).permit(:name, :email)
+      params.fetch(:user).permit(:id, :email)
     end
 end
