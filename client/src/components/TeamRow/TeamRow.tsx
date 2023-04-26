4cd0ac7f858e03ec111a281ec2c8f0dc82e6f311
Juan Pablo <jppd0657@gmail.com>
2023-04-26 09:40:23 -0600

    (client): TeamRow styles changed again

--- client/src/components/TeamRow/TeamRow.tsx
+++ client/src/components/TeamRow/TeamRow.tsx
@@ -8,15 +8,15 @@ interface Props {
 const TeamRow = ({ user }: Props) => {
   return (
     <div className="team-row-general drop-shadow-md">
-      <div className="w-[20%] justify-start ml-[15px]">
+      <div className="team-row-role">
         <p>{user.role}</p>
       </div>
 
-      <div className="w-[70%] justify-start ml-[15px]">
-        <p>{user.name}</p>
+      <div className="team-row-name">
+        <p>{user.name + " " + user.last_name}</p>
       </div>
 
-      <div className="w-[20%] justify-end mr-[15px]">
+      <div className="team-row-button">
         <div className="team-row-profile-button">
           <p className="cursor-pointer">View Profile</p>
         </div>
