1da33e6e6378ba1d349eadb0bba10f93e018cdc1
Alyxhelpme <A01640585@itesm.mx>
2023-04-26 08:45:11 -0600

    cookie settings for chrome same_site: none

--- backend-dashboard-2/config/application.rb
+++ backend-dashboard-2/config/application.rb
@@ -31,8 +31,9 @@ module BackendDashboard2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Cookies, same_site: :none, secure: :true
+    config.middleware.use ActionDispatch::Session::CookieStore, same_site: :none, secure: :true
+    
 
 
     # Only loads a smaller set of middleware suitable for API only apps.
