710d1287a85398aaa2cf3ec85c2ffdb190194696
Emilio Gonzalez <emiliogonmar@Emilios-MacBook-Pro-4.local>
2023-04-27 09:24:03 -0600

    (client): Styling completed

--- client/src/components/ProfileCertificationCard/ProfileCertificationCard.tsx
+++ client/src/components/ProfileCertificationCard/ProfileCertificationCard.tsx
@@ -8,19 +8,20 @@ interface Props {
 
 const ProfileCertificationCard = ({ certificate }: Props) => {
   return (
-    <div className="profile-certifications-card shadow-md rounded-md">
-      <div>
-        <p>{certificate.name}</p>
+    <div className="profile-certifications-card shadow-md rounded-md hover:scale-105 ease-in-out duration-150">
+      <div className="profile-certifications-card-title">
+        <p className="text-2xl mt-auto mb-auto">{certificate.name}</p>
       </div>
-      <div>
-        <div>
-          <p>Expiration Date</p>
-          <p>{certificate.expiration_date}</p>
+
+      <div className="profile-certifications-card-data">
+        <div className="profile-certifications-card-section">
+          <p className="text-sm text-gray-400 mb-3">Expiration Date</p>
+          <p className="text-sm">{certificate.expiration_date}</p>
         </div>
-        <div>
-          <p>Category</p>
+        <div className="profile-certifications-card-section">
+          <p className="text-sm text-gray-400 mb-3">Category</p>
           {certificate.categories.map((category) => {
-            return <p>{category.name}</p>;
+            return <p className="text-sm">{category.name}</p>;
           })}
         </div>
       </div>
