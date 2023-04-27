39e179fd61d7828ed29757c4b2146754950b99b5
Alyxhelpme <A01640585@itesm.mx>
2023-04-27 15:19:11 -0600

    auth_controller (not tested) modified to verify the employee

--- backend-dashboard-2/app/models/user.rb
+++ backend-dashboard-2/app/models/user.rb
@@ -2,6 +2,7 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
   
+  field :id
   field :employee_id
   field :email, type: String
   belongs_to :employees, class_name: "Employee", foreign_key: "employee_id"
