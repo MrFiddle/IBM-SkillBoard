8800b452ef1c8b88893a945dc0eb3ce8f50aa036
Juan Pablo <jppd0657@gmail.com>
2023-05-03 08:35:28 -0600

    (client): Category filter deleted

--- client/src/components/Employees/Employees.tsx
+++ client/src/components/Employees/Employees.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { useState } from "react";
-import Select from "react-select";
 import { BsSearch } from "react-icons/bs";
 import EmployeesTable from "../EmployeesTable/EmployeesTable";
 import { User } from "../../../lib/types";
@@ -11,60 +9,20 @@ interface Props {
 }
 
 const Employees = ({ employees }: Props) => {
-  const [values, setValues] = useState("Category");
   const [searchTerm, setSearchTerm] = React.useState("");
-
-  const options = [
-    {
-      value: "Category",
-      label: "Category",
-    },
-    {
-      value: "Industry",
-      label: "Industry",
-    },
-    {
-      value: "IBM",
-      label: "IBM",
-    },
-    {
-      value: "MyTeam",
-      label: "MyTeam",
-    },
-  ];
 
   return (
     <div className="flex flex-col h-[90vh]">
       <div className="flex flex-row employees-filter">
-        <input
-          type="search"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          onSubmit={() => console.log(searchTerm)}
-          placeholder="Search"
-          className="shadow-md text-sm employees-filter-search"
-        />
-        <Select
-          options={options}
-          defaultValue={{
-            value: "Category",
-            label: "Category",
-          }}
-          onChange={(values) => values && setValues(values.value)}
-          placeholder="Category"
-          className="shadow-md text-sm employees-filter-category"
-          styles={{
-            control: (baseStyles) => ({
-              ...baseStyles,
-              color: "#7A7A7A",
-              border: "none",
-              width: "100%",
-              backgroundColor: "#f4f4f4",
-              fontWeight: 1000,
-              borderRadius: 10,
-            }),
-          }}
-        />
+        <div className="shadow-md text-sm employees-filter-search">
+          <BsSearch />
+          <input
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onSubmit={() => console.log(searchTerm)}
+            placeholder="Search"
+          />
+        </div>
       </div>
       <EmployeesTable employees={employees} />
     </div>
