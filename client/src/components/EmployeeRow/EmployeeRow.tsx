1c80e68ea006716136fd6d81f860ef3eb14f47df
Emilio Gonzalez <emiliogonmar@Emilios-MacBook-Pro-4.local>
2023-05-05 08:00:48 -0600

    (client): employee row view profile button working

--- client/src/components/EmployeeRow/EmployeeRow.tsx
+++ client/src/components/EmployeeRow/EmployeeRow.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { User } from "../../../lib/types";
 import "./EmployeeRow.css";
 
@@ -6,13 +7,20 @@ interface Props {
 }
 
 const EmployeeRow = ({ employee }: Props) => {
+  const navigate = useNavigate();
+
   return (
     <div className="EmployeeRow-container flex drop-shadow-md">
       <p>{employee.name + " " + employee.last_name}</p>
       <p>{employee.email}</p>
       <p>{employee.role}</p>
       <div className="employee-row-button">
-        <div className="employee-row-profile-button">
+        <div
+          className="employee-row-profile-button"
+          onClick={() => {
+            navigate(`/profile/${employee.email.split("@")[0]}`);
+          }}
+        >
           <p className="cursor-pointer">View Profile</p>
         </div>
       </div>
