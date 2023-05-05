be20d78513fdf8c1fc3b0efbeb6c8d986ff36b04
Gael <gaelrodguez@hotmail.com>
2023-05-05 10:45:56 -0600

    (client): Fixing get request to use cookies even when reloading

--- client/src/components/SidebarProfileContainer.tsx
+++ client/src/components/SidebarProfileContainer.tsx
@@ -1,5 +1,7 @@
-import React from "react";
 import SidebarProfile from "./SidebarProfile/SidebarProfile";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "./Loading/Loading";
 
 const id = "cr7id";
 const name = "Cristiano";
@@ -15,7 +17,26 @@ const user = {
 };
 
 const SidebarProfileContainer = () => {
-  return <SidebarProfile user={user} />;
+  const fetchDetails = async () => {
+    const response = await axios.get(
+      `${import.meta.env.VITE_SERVER_URL}/viewer`,
+      { withCredentials: true }
+    );
+    return response.data;
+  };
+
+  const { isLoading, error, data } = useQuery(["viewer"], fetchDetails);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    console.log(error);
+    return <p>error</p>;
+  }
+
+  return <SidebarProfile user={data.employee} />;
 };
 
 export default SidebarProfileContainer;
