0f261b6cb0331e4244cd506947c538dd0aecfdb8
han rodríguez <han@gonzalpi.com>
2023-05-16 13:11:46 -0600

    (devops) remove server and client redeploy.sh scripts

--- client/redeploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-tmux kill-session -t client
-tmux new-session -d -s client
-tmux send-keys -t client 'npm install' C-m
-tmux send-keys -t client 'npm run build' C-m
-tmux send-keys -t client 'cd dist/' C-m
-tmux send-keys -t client 'http-server --cors' C-m
-tmux detach -s client
--- server/redeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-tmux kill-session -t server
-tmux new-session -d -s server
-tmux send-keys -t server 'bundle install' C-m
-tmux send-keys -t server 'bin/rails server -b 0.0.0.0' C-m
-tmux detach -s server
