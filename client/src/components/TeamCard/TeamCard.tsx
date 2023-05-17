65a80a874b60ae19f593c25de314d3f7a08f8c44
Gael <gaelrodguez@hotmail.com>
2023-05-17 09:06:47 -0600

    (client): myteamviewer endpoint connected

--- client/src/components/TeamCard/TeamCard.tsx
+++ client/src/components/TeamCard/TeamCard.tsx
@@ -10,7 +10,7 @@ const TeamCard = ({ team }: Props) => {
   return (
     <div className="team-card-general drop-shadow-md">
       <div className="flex flex-row justify-start w-full">
-        <h2 className="font-bold text-[1.2rem]">{team.team.team_name}</h2>
+        <h2 className="font-bold text-[1.2rem]">{team.team.name}</h2>
       </div>
       <div className="flex flex-col justify-start w-full overflow-y-scroll team-card-employees">
         {team.employees.map((user, index) => (
