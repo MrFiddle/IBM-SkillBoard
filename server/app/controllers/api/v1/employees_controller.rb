49f9ea3bbec2a417a51de3ab5520dc936b64f5f4
Han Rodríguez <alejandro.tzaax@gmail.com>
2023-05-18 22:21:55 -0600

    (backend) typo in employees_controller

--- server/app/controllers/api/v1/employees_controller.rb
+++ server/app/controllers/api/v1/employees_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::EmployeesController < ApplicationController
 
   # GET /employees
   def index
-    @employees = Employee.all.each { |employee| { employee: @employee.info, teams: @employee.teams, certificates: @employee.certificates } }
+    @employees = Employee.all.map { |employee| { employee: @employee.info, teams: @employee.teams, certificates: @employee.certificates } }
 
     render json: @employees
   end
