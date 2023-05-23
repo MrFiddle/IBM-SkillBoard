80f73d2ebf083ebc64e1f8c4555c9041652bf090
Juan Pablo <jppd0657@gmail.com>
2023-05-23 08:52:44 -0600

    (client): fixed a visual bug found in AllEmployeesScreen

--- client/src/components/EmployeesContainer.tsx
+++ client/src/components/EmployeesContainer.tsx
@@ -24,7 +24,7 @@ const EmployeesContainer = () => {
   if (data) {
     return (
       <div>
-        <Employees employees={data} />;
+        <Employees employees={data} />
       </div>
     );
   }
