8a08d383297ce8af044b866e980d14e1ef591c31
Gael <gaelrodguez@hotmail.com>
2023-05-30 10:08:18 -0600

    (client): Debounce Implemented, search starts after 0.5 seconds

--- client/src/components/EmployeesSearchBar/EmployeeSearchBar.tsx
+++ client/src/components/EmployeesSearchBar/EmployeeSearchBar.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { BsSearch } from "react-icons/bs";
 import "./EmployeeSearchBar.css";
+import debounce from "lodash.debounce";
 
 interface Props {
   changeTerm: (change: string) => void;
 }
 
 const EmployeeSearchBar = ({ changeTerm }: Props) => {
+  const debouncedOnChange = debounce(changeTerm, 500);
+
   return (
     <div className="flex flex-row employees-filter">
       <div className="shadow-md text-sm employees-filter-search">
         <BsSearch />
         <input
-          onChange={(e) => changeTerm(e.target.value)}
+          onChange={(e) => debouncedOnChange(e.target.value)}
           placeholder="Search"
         />
       </div>
