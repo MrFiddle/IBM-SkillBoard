cf50779f711f4753d06c412bb2f5ca52c2935ab2
Juan Pablo <jppd0657@gmail.com>
2024-01-24 17:37:39 -0700

    myteams screen data updated

--- client/src/components/TeamTableContainer.tsx
+++ client/src/components/TeamTableContainer.tsx
@@ -4,23 +4,92 @@ import { useQuery } from "@tanstack/react-query";
 import Loading from "./Loading/Loading";
 
 const TeamTableContainer = () => {
-  const fetchDetails = async () => {
-    const response = await axios.get(
-      `${import.meta.env.VITE_SERVER_URL}/employees/myteamviewer`,
-      { withCredentials: true }
-    );
-    return response.data;
+  // export interface Team {
+  //   id: string;
+  //   name: string;
+  // }
+
+  // export interface TeamWithManager {
+  //   team: Team;
+  //   manager: User;
+  // }
+
+  // export interface TeamWithEmployeesManager {
+  //   team: Team;
+  //   employees: User[];
+  //   manager: User;
+  // }
+
+  // export interface User {
+  //   id: string;
+  //   name: string;
+  //   last_name: string;
+  //   email: string;
+  //   role: string;
+  // }
+
+  const data = {
+    teams: [
+      {
+        team: {
+          id: "team1",
+          name: "Frontend",
+        },
+        employees: [
+          {
+            id: "user2",
+            name: "John",
+            last_name: "Doe",
+            email: "johndoe@ibm.com",
+            role: "Frontend Developer",
+          },
+          {
+            id: "user3",
+            name: "Jane",
+            last_name: "Doe",
+            email: "janedoe@ibm.com",
+            role: "Frontend Developer",
+          },
+        ],
+        manager: {
+          id: "user1",
+          name: "Juan Pablo",
+          last_name: "Perez Duran",
+          email: "jppd0657@gmail.com",
+          role: "Manager",
+        },
+      },
+      {
+        team: {
+          id: "team2",
+          name: "Backend",
+        },
+        employees: [
+          {
+            id: "user4",
+            name: "Hugo",
+            last_name: "Salmon",
+            email: "hugosalmon@ibm.com",
+            role: "Backend Developer",
+          },
+          {
+            id: "user5",
+            name: "Hermann",
+            last_name: "Mijangos",
+            email: "hermannmij@ibm.com",
+            role: "Backend Developer",
+          },
+        ],
+        manager: {
+          id: "user4",
+          name: "Manola",
+          last_name: "Zaragoza",
+          email: "manolazaragoza@ibm.com",
+          role: "Manager",
+        },
+      },
+    ],
   };
-
-  const { isLoading, error, data } = useQuery(["myteamviewer"], fetchDetails);
-
-  if (isLoading || !data) {
-    return <Loading type={true} mainColor={false} />;
-  }
-
-  if (error) {
-    return <p>error</p>;
-  }
 
   return <TeamTable teams={data.teams} />;
 };
