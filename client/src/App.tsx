d05b1f12d4450971c36d151dd9c2a70402065246
Juan Pablo <jppd0657@gmail.com>
2024-01-24 18:24:00 -0700

    idk if i fixed it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,13 @@
-import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import RoutesAvailable from "./RoutesAvailable";
 import "./App.css";
-import UserProvider from "./contexts/UserContext";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 function App() {
-  const queryClient = new QueryClient({
-    logger: {
-      log: () => {},
-      warn: () => {},
-      error: () => {},
-    },
-  });
-
   return (
     <div className="ibm-font">
-      <QueryClientProvider client={queryClient}>
-        <UserProvider>
-          <BrowserRouter>
-            <RoutesAvailable />
-          </BrowserRouter>
-        </UserProvider>
-      </QueryClientProvider>
+      <BrowserRouter>
+        <RoutesAvailable />
+      </BrowserRouter>
     </div>
   );
 }
